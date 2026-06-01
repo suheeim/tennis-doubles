d6974c3400e109eb478811f4730a941087e9fe8a
suheeim <suhee.im@gmail.com>
2026-06-01 17:51:22 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'tennis-doubles-v1';
+var urlsToCache = [
+  '/tennis-doubles/',
+  '/tennis-doubles/index.html',
+  '/tennis-doubles/index_en.html',
+  '/tennis-doubles/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+});
